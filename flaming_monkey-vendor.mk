ec54b831444ccb93fc35f5420ed3459c09b9a159
Craig Gomez <craigacgomez@gmail.com>
2013-10-11 07:38:29 -0700

    Remove unneeded init script

--- flaming_monkey-vendor.mk
+++ flaming_monkey-vendor.mk
@@ -2,7 +2,6 @@
 PRODUCT_COPY_FILES += \
     vendor/flaming_monkey/sysinit/sysinit:system/bin/sysinit \
     vendor/flaming_monkey/sysinit/00banner:system/etc/init.d/00banner \
-    vendor/flaming_monkey/sysinit/70storage:system/etc/init.d/70storage \
     vendor/flaming_monkey/sysinit/90userinit:system/etc/init.d/90userinit
 
 # Bootanimation
